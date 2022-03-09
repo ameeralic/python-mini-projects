93f8f4879540239aef25bfea2b5f5692beea0f89
ameeralic <ameeralic95@gmail.com>
2022-03-09 17:06:08 +0530

    madlib final

---
+++ madlib/main.py
@@ -0,0 +1,7 @@
+adj=input('Enter an adjuctive: ')
+verb1=input('Enter a verb: ')
+verb2=input('Enter a verb: ')
+name=input('Enter a name: ')
+madlib=f'Computer programming {adj}, \
+becuase I love {verb1}. stay hydrated and {verb2} to {name}'
+print(madlib)
