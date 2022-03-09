14a3d80b0acaf6495164b4ae3a9c18838154777e
ameeralic <ameeralic95@gmail.com>
2022-03-09 17:08:51 +0530

    madlib final

--- madlib/main.py
+++ madlib/main.py
@@ -3,5 +3,5 @@ verb1=input('Enter a verb: ')
 verb2=input('Enter a verb: ')
 name=input('Enter a name: ')
 madlib=f'Computer programming {adj}, \
-becuase I love {verb1}. stay hydrated and {verb2} to {name}'
+GOODbecuase I love {verb1}. stay hydrated and {verb2} to {name}'
 print(madlib)
